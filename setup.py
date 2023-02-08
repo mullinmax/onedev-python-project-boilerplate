8706853f6645353fed38538e6ecff1627111bb97
maxwell mullin <mullinmax@gmail.com>
2023-02-07 21:59:21 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Project Name',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    description='A brief description of your project',
+    author='Maxwell Mullin',
+    author_email='mullinmax@gmail.com',
+    install_requires=[
+        pytest
+    ],
+	extras_require={
+        'build': [
+            # dependencies required for building your package
+        ],
+        'test': [
+            # dependencies required for testing your package
+            'pytest'
+        ],
+    },
+)
